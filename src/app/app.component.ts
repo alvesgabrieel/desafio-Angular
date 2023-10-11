882a7416acdb09664332b3bcaa3dd2d7c8ee44d6
Gabriel Alves <gabriel_alvesqm@hotmail.com>
2023-10-11 02:27:08 -0300

    add main content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'DevChuva';
+  // title = 'DevChuva';
+  isExpanded = false;
+  buttonText = 'Ver mais';
+
+  toggleContent() {
+    this.isExpanded = !this.isExpanded;
+    this.buttonText = this.isExpanded ? 'Ver menos' : 'Ver mais';
+  }
 }
