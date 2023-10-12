2645fe58ba23845f701b5ba507aaf009635f3a0e
Gabriel Alves <gabriel_alvesqm@hotmail.com>
2023-10-12 11:55:41 -0300

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,8 +10,18 @@ export class AppComponent {
   isExpanded = false;
   buttonText = 'Ver mais';
 
+  showForm = false;
+
   toggleContent() {
     this.isExpanded = !this.isExpanded;
     this.buttonText = this.isExpanded ? 'Ver menos' : 'Ver mais';
+  }
+
+  toggleForm() {
+    this.showForm = !this.showForm;
+  }
+
+  submitForm(event: Event) {
+    event.preventDefault();
   }
 }
